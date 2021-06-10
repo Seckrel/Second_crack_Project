5379d320a773ce3cc8bf97d88687d1da5e5f04c4
Aayam <aayam.oza@gmail.com>
2021-06-10 21:12:00 +0545

    project done

--- src/components/recycle/SpecialMenuComponent.jsx
+++ src/components/recycle/SpecialMenuComponent.jsx
@@ -12,84 +12,84 @@ import Button from '@material-ui/core/Button';
 import CreditImg from './CreditImgComponent';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles'
-import { useStylesp20  } from '../../materialUI/Styles'
+import { useStylesp20 } from '../../materialUI/Styles'
 import { useState, Fragment } from 'react'
 
 
 const useHeadingStyles = makeStyles((theme) => ({
     root: {
-      fontWeight: 400,
-      fontFamily: "'Old Growth','Arial','sans-serif'",
-      fontSize: 15,
-      backgroundColor: theme.palette.background.paper,
-      width: "auto",
+        fontWeight: 400,
+        fontFamily: "'Old Growth','Arial','sans-serif'",
+        fontSize: 15,
+        backgroundColor: theme.palette.background.paper,
+        width: "auto",
     },
-  }));
+}));
 
 const useMenuHeadStyles = makeStyles((theme) => ({
     root: {
-      fontWeight: 400,
-      fontFamily: "'Brandon Grotesque','Arial','sans-serif'",
-      fontSize: 12,
-      width: "auto",
+        fontWeight: 400,
+        fontFamily: "'Brandon Grotesque','Arial','sans-serif'",
+        fontSize: 12,
+        width: "auto",
     },
-  }));
+}));
 
 
-const MenuItemsView = ({menuItem}) => {
+const MenuItemsView = ({ menuItem }) => {
     // const items = hotDrinkActive?menuItem.hotDrinks:menuItem.breakfast;
     return (
         <Grid container spacing={3}>
             {menuItem.map(item => (
                 <Fragment>
                     <Grid item xs={12}>
-                        <Typography 
-                            variant="h6" 
+                        <Typography
+                            variant="h6"
                             component="h3"
-                            style={{fontFamily: "'Brandon Grotesque','Arial','sans-serif'"}}
+                            style={{ fontFamily: "'Brandon Grotesque','Arial','sans-serif'" }}
                         >
                             {item.name}
                         </Typography>
-                        <Typography 
-                            variant="caption" 
+                        <Typography
+                            variant="caption"
                             component="span"
-                            style={{fontFamily: "'courier-std', 'sans-serif'"}}
+                            style={{ fontFamily: "'courier-std', 'sans-serif'" }}
                         >
-                            {item.receipe?item.receipe:""}
+                            {item.receipe ? item.receipe : ""}
                         </Typography>
                         <Divider variant={'middle'} />
                     </Grid>
-                    
+
                 </Fragment>
             ))}
         </Grid>
     )
 }
 
-const FullMenuBtn = ({link}) => {
-    if (link !== '/'){
+const FullMenuBtn = ({ link }) => {
+    if (link !== '/') {
         return (
             <div></div>
         )
     }
     return (
         <Box m={2}>
-                    <Grid spacing={3}>
-                        <Grid container justify={"center"} item xs={12}>
-                        <Button variant="contained" disableElevation>
-                            <Link 
-                                to={'/menu'}
-                                style={{ 
-                                    textDecoration: 'none', 
-                                    color: 'inherit' 
-                                }} 
-                            >
-                                Show Full Menu
+            <Grid spacing={3}>
+                <Grid container justify={"center"} item xs={12}>
+                    <Button variant="contained" disableElevation>
+                        <Link
+                            to={'/menu'}
+                            style={{
+                                textDecoration: 'none',
+                                color: 'inherit'
+                            }}
+                        >
+                            Show Full Menu
                             </Link>
-                        </Button>
-                        </Grid>
-                    </Grid>
-                </Box>
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
@@ -106,8 +106,7 @@ const MenuView = (props) => {
     const toggleClass = (id) => {
         subTypes.forEach(item => {
             item.active = false;
-            if (id === item.id) 
-            {   
+            if (id === item.id) {
                 item.active = true;
                 setHotDrinkActive(!hotDrinkActive);
                 setActiveMenuItem(item.menuItem);
@@ -117,35 +116,35 @@ const MenuView = (props) => {
 
     const headCols = () => {
         console.log(viewType);
-        if (viewType === 'mobile' || viewType === 'tablet'){
+        if (viewType === 'mobile' || viewType === 'tablet') {
             return 2
         }
         return 3
     }
 
     const itemCols = () => {
-        if (headCols() === 2){
-            return viewType === 'mobile'?2:1
+        if (headCols() === 2) {
+            return viewType === 'mobile' ? 2 : 1
         }
         return 1
     }
-    
+
     return (
         <Container disableGutters>
             <Paper
-                elevation={3} 
-                classes={""} 
+                elevation={3}
+                classes={""}
                 className={classes.padding20}
                 square={true}
             >
-                
+
                 <Box component="div" m={1}>
                     <GridList spacing={10} cellHeight={180} cols={headCols()} className={classes.gridList}>
-                        <GridListTile key="Subheader" cols={headCols()}style={{ height: 'auto' }}>
+                        <GridListTile key="Subheader" cols={headCols()} style={{ height: 'auto' }}>
                             <ListSubheader component="div">
-                                <Typography 
-                                    variant="h5" 
-                                    classes={headTypography} 
+                                <Typography
+                                    variant="h5"
+                                    classes={headTypography}
                                     component="h2"
                                 >
                                     {menuDetails.menuType.toLowerCase()}
@@ -153,36 +152,36 @@ const MenuView = (props) => {
                             </ListSubheader>
                         </GridListTile>
                         {subTypes.map((tile) => (
-                        <GridListTile 
-                            cellHeight={160} 
-                            key={tile.id} 
-                            cols={itemCols()}
-                            onClick={!tile.active?() => toggleClass(tile.id):null}
-                            className={tile.active?'btn-weather':'btn-faded'}
-                            
-                        >
-                            <CreditImg credit={tile.credit} src={tile.backgroundImgSrc} altText={tile.altText} />
-                            <GridListTileBar
-                                onClick={!tile.active?() => toggleClass(tile.id):null}
-                                title={
-                                    <Typography
-                                        variant="h6"
-                                        component="h3"
-                                        classes={specialMenuHead}
-                                    >
-                                        {tile.tileText}
-                                    </Typography>
-                                }
-                            />
-                            
-                        </GridListTile>                        
+                            <GridListTile
+                                cellHeight={160}
+                                key={tile.id}
+                                cols={itemCols()}
+                                onClick={!tile.active ? () => toggleClass(tile.id) : null}
+                                className={tile.active ? 'btn-weather' : 'btn-faded'}
+
+                            >
+                                <CreditImg credit={tile.credit} src={tile.backgroundImgSrc} altText={tile.altText} />
+                                <GridListTileBar
+                                    onClick={!tile.active ? () => toggleClass(tile.id) : null}
+                                    title={
+                                        <Typography
+                                            variant="h6"
+                                            component="h3"
+                                            classes={specialMenuHead}
+                                        >
+                                            {tile.tileText}
+                                        </Typography>
+                                    }
+                                />
+
+                            </GridListTile>
                         ))}
                     </GridList>
                 </Box>
                 <Box component="div" m={2}>
-                    <MenuItemsView menuItem={activeMenuItem}/>
+                    <MenuItemsView menuItem={activeMenuItem} />
                 </Box>
-                <FullMenuBtn link={match.url}/>
+                <FullMenuBtn link={match.url} />
             </Paper>
         </Container>
     )
